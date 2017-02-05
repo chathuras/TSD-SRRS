91be5da359c81d61af10398e2cf2a214548ac879
Chathura Sandeepa <chathura@eight25media.com>
2017-02-05 12:17:48 +0530

    acl added

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
